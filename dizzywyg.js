59367ca8789eebb61dba255d5a9c72043326361e
Brian Lonsdorf <brian@trnsfr.com>
2008-11-15 14:43:17 -0600

    toolbar checks state and enables buttons, added DizzyElement class which is for defining

--- dizzywyg.js
+++ dizzywyg.js
@@ -80,8 +80,8 @@ var FireFox = Class.create(Dizzywyg, {
     
     add_event_listeners: function() {
       self = this;
-      this.iframe.contentWindow.document.addEventListener("mouseup", function(){self.toolbar.check_state(self); return true;}, false);
-  		this.iframe.contentWindow.document.addEventListener("keyup", function(){self.toolbar.check_state(self); return true;}, false);
+      this.iframe.contentWindow.document.addEventListener("mouseup", function(){self.toolbar.check_state(); return true;}, false);
+  		this.iframe.contentWindow.document.addEventListener("keyup", function(){self.toolbar.check_state(); return true;}, false);
   		this.iframe.contentWindow.document.addEventListener("keydown", function(e){self.detect_paste(e); return true;}, false);
     },
     
@@ -96,6 +96,12 @@ var FireFox = Class.create(Dizzywyg, {
     
     get_selection: function() {
       return this.iframe.contentWindow.getSelection();
+    },
+
+    get_selection_parent: function(selection) {
+     parent = selection.getRangeAt(0).commonAncestorContainer;
+     while (parent.nodeType == 3)	{	parent = parent.parentNode;	}
+     return parent;
     }
 });
 // ==============================
@@ -108,8 +114,8 @@ var IE = Class.create(Dizzywyg, {
     
     add_event_listeners: function() {
       self = this;
-  		this.iframe.contentWindow.document.attachEvent("onmouseup", function(){self.toolbar.check_state(self); return true;});
-  		this.iframe.contentWindow.document.attachEvent("onkeyup", function(){self.toolbar.check_state(self); return true;});
+  		this.iframe.contentWindow.document.attachEvent("onmouseup", function(){self.toolbar.check_state(); return true;});
+  		this.iframe.contentWindow.document.attachEvent("onkeyup", function(){self.toolbar.check_state(); return true;});
   		this.iframe.contentWindow.document.attachEvent("onkeydown", function(e){self.detect_paste(e); return true;}, false);
     },
     
@@ -118,7 +124,13 @@ var IE = Class.create(Dizzywyg, {
     },
     
     get_selection: function() {
-      return this.iframe.contentWindow.document.selection.createRange().text;
+      return this.iframe.contentWindow.document.selection;
+    },
+    
+    get_selection_parent: function(selection) {
+     parent = selection.createRange().parentElement();
+     while (parent.nodeType == 3)	{	parent = parent.parentNode;	}
+     return parent;
     }
 });
 // ==============================
@@ -126,18 +138,33 @@ var IE = Class.create(Dizzywyg, {
 // =======Toolbar CLASS=======
 var Toolbar = Class.create({
     initialize: function(editor) {
+      this.buttons = new Array();
       this.editor = editor;
       this.create_elements();
     },
     
-    check_state: function() {
-      console.log('checked');
+    check_state: function(resubmit) {
+    	var self = this;
+    	
+      /* Allow browser to update selection before using the selection */
+      if (!resubmit) { setTimeout(function(){self.check_state(true); return true;}, 500) };
+    	this.buttons.each(function(b){ b.li.removeClassName('on') })
+    	
+      var selection = this.editor.get_selection();
+      var parent = this.editor.get_selection_parent(selection);
+      var element = new DizzyElement(parent, self);
+      
+      element.get_style().each(function(e){ 
+        self.buttons.detect(function(b) { return b.kind == e }).set_state('on')
+      });
+      // console.log(style);
+      
     },
     
     create_elements: function() {
       var self = this;
       this.ul = Builder.node('ul', {class:"dizzy_toolbar"});
-      ['bold', 'italic'].each(function(e){ new ToolbarButton(self, e) });
+      ['bold','italic','orderedlist','unorderedlist','image','link','html'].each(function(e){ self.buttons.push(new ToolbarButton(self, e)) });
       this.editor.iframe.insert({before:this.ul});
     }
     
@@ -154,30 +181,68 @@ var ToolbarButton = Class.create({
       this.a.onclick = this.edit.bindAsEventListener(this);
     },
     
-    bold: function() {
-      var selection = this.editor.get_selection();
-      alert(selection);
+    action: function() {
+      return this.is_list() ? 'insert'+this.kind : this.kind;
     },
     
     check_state: function() {
-      console.log('checked');
+			if (this.is_list()) { this.set_state("off"); }
+			this.is_on() ? this.set_state("on") : this.set_state("off");
     },
-    
+
     create_elements: function() {
-      this.li = Builder.node('li', {class:this.kind+' button'}, [
-        this.a  = Builder.node('a', this.kind)
-      ]);
+      this.li = Builder.node('li', {class:this.kind+' button'}, [ this.a  = Builder.node('a', this.kind) ]);
       this.toolbar.ul.insert(this.li);
     },
     
     edit: function(){
-      return eval('this.'+this.kind+'()');
+      this.editor.iframe.contentWindow.document.execCommand(this.action(), false, null);
+      this.check_state();
     },
     
-    italic: function() {
-      alert('italic!');
+    is_list: function(){
+      return this.kind.match(/list/);
+    },
+    
+    is_on: function() {
+      return this.editor.iframe.contentWindow.document.queryCommandState(this.action(), false, null);
+    },
+    
+    set_state: function(state) {
+      state == 'on' ? this.li.addClassName("on") : this.li.removeClassName("on")
+      this.editor.iframe.contentWindow.focus();
     }
     
+});
+// ==================================
+// ==================================
+// =======ToolbarButton CLASS=======
+var DizzyElement = Class.create({
+    initialize: function(element, toolbar) {
+      this.element = element;
+      this.toolbar = toolbar;
+    },
+    
+    name: function(){
+      this.element.nodeName.toLowerCase();
+    },
+    
+    get_style: function() {
+      var self = this;
+      var style = this.element.getAttribute("style");
+      if (!style) { return []; }
+      var styles = style.split(';');
+      styles = styles.reject(function(s) s == '');
+      return styles.map(function(s){ return self.style_hash().get(s.strip()); });
+    },
+    
+    style_hash: function() {
+      if (typeof style_hash != "undefined") { return style_hash; }
+      style_hash = new Hash();
+      style_hash.set("font-weight: bold", 'bold');
+      style_hash.set("font-style: italic", 'italic');
+      return style_hash;
+    }
 });
 
 document.observe('dom:loaded', function() {
